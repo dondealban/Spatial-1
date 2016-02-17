5365ab053164a31783fc223defd4096f7d90c847
ScouserInTrousers <ScouserInTrousers@users.noreply.github.com>
2016-02-17 13:45:19 -0700

    Up through part 4

--- HW3.R
+++ HW3.R
@@ -1,3 +1,4 @@
+#https://github.com/ScouserInTrousers/Spatial.git
 # Install packages and mask functions --------------------
 rm(list=ls())
 list.of.packages <- c("dplyr", "stringr","ggmap","spatstat","fields","gstat","lattice")
@@ -7,11 +8,14 @@ lapply(list.of.packages, require, character.only = TRUE)
 select <- dplyr::select
 rm(list.of.packages,new.packages)
 # Functions to be used --------------------
-svgm <- function(df){
-  x <- df %>% select(one_of("x","lon","long"))
-  y <- df %>% select(one_of("y","lat","latt"))
-  z <- dff[,3]
-  gstat::variogram(z~1, loc = ~x+y, data=df,cloud=T)
+svgm <- function(df,bin=NULL,cut=1){
+  par(mfrow=c(1,2))
+  x <- df[,1]
+  y <- df[,2]
+  d <- as.matrix(dist(cbind(x,y)))
+  z <- df[,3]
+  p <- gstat::variogram(z~1, loc = ~x+y, data=df,cloud=T,cutoff=max(d)/cut,width=bin,alpha=c(0,90))
+  plot(p,ylab=expression((Z(s[i])-Z(s[j]))^2))
 }
 # Loading data --------------------
 WIPP_transmissivity <- read.table("E:/MATH532/HW3/WIPP_transmissivity.txt", header=TRUE, quote="\"")
@@ -21,7 +25,15 @@ ggplot(data=WIPP_transmissivity,aes(x=x,y=y)) +
   geom_point() +
   geom_text(data = WIPP_transmissivity, aes(x,y, label = seq(1:length(x))), hjust = 2)
 #Plot the semivariogram cloud for ALL pairs of points 
-vgm.cloud <- variogram(logT~1, loc = ~x+y, data=WIPP_transmissivity,cloud=T)
+svgm(WIPP_transmissivity)
 
 #How does the semivariogram cloud change when the large value of -10.12 is removed?
 wipp <- WIPP_transmissivity[-which(WIPP_transmissivity$logT < -10),]
+svgm(wipp)
+
+#Investigate anisotropy in the data 
+d <- as.matrix(dist(cbind(wipp$x,wipp$y)))
+vg1=variogram(logT~1, ~x+y,data=wipp,cutoff=max(d)/2,width=4)
+plot(vg1,pch=19,col=1,ylab=expression(paste("Estimated ",gamma(h),sep="")))
+
+
